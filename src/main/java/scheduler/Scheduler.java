20ddd563d9185d0101866a7f33ea1f328e02a0c8
Saewon-Min <minsw626@naver.com>
2021-07-06 19:42:31 +0900

    first commit

---
+++ src/main/java/scheduler/Scheduler.java
@@ -0,0 +1,37 @@
+package scheduler;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+
+	/*
+	cron 설정
+	형식]							
+			초 	 분   시   일   월  요일      연도
+		=> 0~59 0~59 0~24 1~31 1~12  0~6  연도(생략가능)
+	
+	요일의 경우 0(일요일) ~ 6(토요일)
+		
+	설정 예] 
+		(cron= "0/10 * * * * *") => 매 10초마다 호출
+		(cron= "0 45 11 * * *") => 매일 11시 45분에 호출
+	 
+	 
+	 */
+	@Scheduled(cron= "0/10 * * * * *")
+	public void autoUpdate() {
+		
+		// 시간에 대한 포맷 지정
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+		
+		// 현재 시간을 위에서 설정한 포맷으로 변경 후 출력
+		String nowTime = LocalTime.now().format(formatter);
+		System.out.println("스케줄러 호출 : "+nowTime);
+		
+	}
+}
